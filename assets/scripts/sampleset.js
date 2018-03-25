120a3fb3b511c07acb8acad677f7a087875f45f8
anna0312 <annabremner@gmail.com>
2018-03-25 18:44:52 -0400

    Added more formatting and items to the product grid prototype
    Added "add to cart" buttons, qty, and image placeholders to product
    listing prototype. We can change the look/layout of the cards. This is
    just placeholder stuff.
    
    I have not yet added the modal window...

--- assets/scripts/sampleset.js
+++ assets/scripts/sampleset.js
@@ -2,62 +2,62 @@ const sampleItems = {
   'items': [{
     'id': 1,
     'name': 'item1',
-    'description': 'item1',
+    'description': 'item1Lorem ipsum dolor sit amet, consectetur adipiscing elit ut description',
     'price': '5'
   }, {
     'id': 2,
     'name': 'item2',
-    'description': 'item2 description',
+    'description': 'item2Lorem ipsum dolor sit amet, consectetur adipiscing elit ut description',
     'price': '5'
   }, {
     'id': 3,
     'name': 'item3',
-    'description': 'item3 description',
+    'description': 'item3Lorem ipsum dolor sit amet, consectetur adipiscing elit ut description',
     'price': '5'
   }, {
     'id': 4,
     'name': 'item4',
-    'description': 'item4 description',
+    'description': 'item4Lorem ipsum dolor sit amet, consectetur adipiscing elit ut description',
     'price': '5'
   }, {
     'id': 5,
     'name': 'item5',
-    'description': 'item5 description',
+    'description': 'item5Lorem ipsum dolor sit amet, consectetur adipiscing elit ut description',
     'price': '5'
   }, {
     'id': 6,
     'name': 'item6',
-    'description': 'item6 description',
+    'description': 'item6Lorem ipsum dolor sit amet, consectetur adipiscing elit ut description',
     'price': '5'
   }, {
     'id': 7,
     'name': 'item7',
-    'description': 'item7 description',
+    'description': 'item7Lorem ipsum dolor sit amet, consectetur adipiscing elit ut description',
     'price': '5'
   }, {
     'id': 8,
     'name': 'item8',
-    'description': 'item8 description',
+    'description': 'item8Lorem ipsum dolor sit amet, consectetur adipiscing elit ut description',
     'price': '5'
   }, {
     'id': 9,
     'name': 'item9',
-    'description': 'item9 description',
+    'description': 'item9Lorem ipsum dolor sit amet, consectetur adipiscing elit ut description',
     'price': '5'
   }, {
     'id': 10,
     'name': 'item10',
-    'description': 'item10 description',
+    'description': 'item10Lorem ipsum dolor sit amet, consectetur adipiscing elit ut description',
     'price': '5'
   }, {
     'id': 11,
     'name': 'item11',
-    'description': 'item11 description',
+    'description': 'item11Lorem ipsum dolor sit amet, consectetur adipiscing elit ut description',
     'price': '5'
   }, {
     'id': 12,
     'name': 'item12',
-    'description': 'item12 description',
+    'description': 'item12Lorem ipsum dolor sit amet, consectetur adipiscing elit ut description',
     'price': '5'
   }]
 }
